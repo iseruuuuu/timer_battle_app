5b16f8c1eaa9480a281199b1836d9185d3a4eba3
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-08-06 22:23:25 +0900

    fix: ボタンの位置や大きさを変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timer_battle_app/timer_battle/timer_battle_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,74 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Start'),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child:  const Text('Stop'),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Reset'),
-                ),
-              ],
-            ),
-            Text('00:00:00'),
-            Text('00:00:00'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Start'),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child:  const Text('Stop'),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Reset'),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+      home: const TimerBattleScreen(),
     );
   }
 }
---
+++ lib/timer_battle/timer_battle_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class TimerBattleScreen extends StatelessWidget {
+  const TimerBattleScreen ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+           const SizedBox(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Start'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child:  const Text('Stop'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Reset'),
+                ),
+              ],
+            ),
+            Text(
+              '00:00:00',
+              style: TextStyle(
+                fontSize: size.width / 7,
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              '00:00:00',
+              style: TextStyle(
+                fontSize: size.width / 7,
+                color: Colors.black,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Start'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child:  const Text('Stop'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Reset'),
+                ),
+              ],
+            ),
+            const SizedBox(),
+          ],
+
+        ),
+      ),
+    );
+  }
+}
