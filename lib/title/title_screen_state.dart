0a3f0ca95029d719f4c6ed6c36d8e5f951a4b367
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-08-07 20:26:07 +0900

    add: urlを追加

--- lib/title/title_screen_state.dart
+++ lib/title/title_screen_state.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:state_notifier/state_notifier.dart';
 import 'package:timer_battle_app/timer_battle/timer_battle_screen.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 part 'title_screen_state.freezed.dart';
 
@@ -28,8 +29,10 @@ class TitleScreenController extends StateNotifier<TitleScreenState> with Locator
     );
   }
 
-  void onTapOtherAPP() {
+  void onTapOtherAPP() async{
    //TODO 別の画面遷移
+   String _url = 'https://apps.apple.com/jp/developer/ryutaro-iseki/id1544199624#see-all/i-phone-apps';
+   await canLaunch(_url) ? await launch(_url) : throw 'Could not launch $_url';
   }
 
 }
